54a61bd24b90b923a5b555a66f0cd753de53fa8a
Ketan-Chaudhary <ketanchaudharypoonia@gmail.com>
2025-01-20 11:08:51 +0530

    v3.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Ola Clone',
+  description: 'Clone of the ola electric',
 }
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import { NotificationBar } from "../components/notification-bar"
+import  Home  from "../page"
 
 export default function SyntheticV0PageForDeployment() {
-  return <NotificationBar />
+  // return <NotificationBar />
+  return <Home/>
 }
